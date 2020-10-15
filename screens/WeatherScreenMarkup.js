e563a988109bf6975d5b75eb0f5cd993648abab9
Your Name <you@example.com>
2020-10-15 15:05:46 +0200

    notification bug fix and some style changes

--- screens/WeatherScreenMarkup.js
+++ screens/WeatherScreenMarkup.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, ScrollView, Text, StyleSheet, SafeAreaView, RefreshControl } from 'react-native'
+import { View, ScrollView, Text, StyleSheet, SafeAreaView, RefreshControl, ImageBackground } from 'react-native'
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -7,9 +7,12 @@ WeatherScreenMarkup = (props) => {
 
     return (
         <SafeAreaView style={{ flex: 1 }}>
+            
+            <ImageBackground source={require('../assets/bgw.jpg')} style={styles.backgroundImage}>
             <ScrollView contentContainerStyle={styles.container} bounces={false} refreshControl={
                 <RefreshControl refreshing={props.refreshing} onRefresh={props.onRefresh} />
             }>
+                            
 
                 <View style={{ alignItems: 'center' }}>
                     <Icon name="home-city-outline" size={hp('10%')} color="black" />
@@ -23,7 +26,9 @@ WeatherScreenMarkup = (props) => {
                 <View>
                     <Text style={{ fontSize: hp('4%') }}>{props.skyText}</Text>
                 </View>
+               
             </ScrollView>
+            </ImageBackground>
         </SafeAreaView>
     );
 }
@@ -38,7 +43,14 @@ const styles = StyleSheet.create({
         paddingLeft: wp('5%'),
         paddingRight: wp('5%'),
         paddingBottom: hp('5%'),
-        backgroundColor: '#87ceeb',
+    },
+    backgroundImage: {
+        flex:1,
+        width: '100%',
+        height: '100%',
+        justifyContent: "center",
+        alignItems: "center",
+        opacity: 0.7
     },
 })
 
